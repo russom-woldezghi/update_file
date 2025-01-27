914b32662e826416c33e39cb68b835125f270b03
Russom Woldezghi <russom.woldezghi@adhocteam.us>
2025-01-27 12:10:03 -0500

    Scaffolding for field type

---
+++ src/Plugin/Field/FieldType/UpdateFileItem.php
@@ -0,0 +1,142 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\update_file\Plugin\Field\FieldType;
+
+use Drupal\Component\Utility\Random;
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Field\FieldItemBase;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\TypedData\DataDefinition;
+
+/**
+ * Defines the 'update_file_upload' field type.
+ *
+ * @FieldType(
+ *   id = "update_file_upload",
+ *   label = @Translation("Update File Upload(s)"),
+ *   description = @Translation("Some description."),
+ *   default_widget = "string_textfield",
+ *   default_formatter = "string",
+ * )
+ */
+final class UpdateFileItem extends FieldItemBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultStorageSettings(): array {
+    $settings = ['foo' => ''];
+    return $settings + parent::defaultStorageSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function storageSettingsForm(array &$form, FormStateInterface $form_state, $has_data): array {
+    $element['foo'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Foo'),
+      '#default_value' => $this->getSetting('foo'),
+      '#disabled' => $has_data,
+    ];
+    return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultFieldSettings(): array {
+    $settings = ['bar' => ''];
+    return $settings + parent::defaultFieldSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fieldSettingsForm(array $form, FormStateInterface $form_state): array {
+    $element['bar'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Bar'),
+      '#default_value' => $this->getSetting('bar'),
+    ];
+    return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isEmpty(): bool {
+    return match ($this->get('value')->getValue()) {
+      NULL, '' => TRUE,
+      default => FALSE,
+    };
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition): array {
+
+    // @DCG
+    // See /core/lib/Drupal/Core/TypedData/Plugin/DataType directory for
+    // available data types.
+    $properties['value'] = DataDefinition::create('string')
+      ->setLabel(t('Text value'))
+      ->setRequired(TRUE);
+
+    return $properties;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConstraints(): array {
+    $constraints = parent::getConstraints();
+
+    $constraint_manager = $this->getTypedDataManager()->getValidationConstraintManager();
+
+    // @DCG Suppose our value must not be longer than 10 characters.
+    $options['value']['Length']['max'] = 10;
+
+    // @DCG
+    // See /core/lib/Drupal/Core/Validation/Plugin/Validation/Constraint
+    // directory for available constraints.
+    $constraints[] = $constraint_manager->create('ComplexData', $options);
+    return $constraints;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function schema(FieldStorageDefinitionInterface $field_definition): array {
+
+    $columns = [
+      'value' => [
+        'type' => 'varchar',
+        'not null' => FALSE,
+        'description' => 'Column description.',
+        'length' => 255,
+      ],
+    ];
+
+    $schema = [
+      'columns' => $columns,
+      // @todo Add indexes here if necessary.
+    ];
+
+    return $schema;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function generateSampleValue(FieldDefinitionInterface $field_definition): array {
+    $random = new Random();
+    $values['value'] = $random->word(mt_rand(1, 50));
+    return $values;
+  }
+
+}
